f9514056664cf3fea140923044b36007fef6867a
idbetwill <79185255+idbetwill@users.noreply.github.com>
2024-12-02 19:48:17 -0500

    Add new dependece to deploy netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
-// Import the Vercel adapter
-import vercel from '@astrojs/vercel/serverless';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: "server",
+  adapter: netlify(),
 });
